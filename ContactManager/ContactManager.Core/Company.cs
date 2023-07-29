2e8122e37cb816782df75f976a8019fd4b942ddd
Silverback2023 <121978344+Silverback2023@users.noreply.github.com>
2023-07-29 01:59:32 -0700

    Add files via upload

---
+++ ContactManager/ContactManager.Core/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Core
+{
+    public class Company : Contact
+    {
+        public List<Individual> Employees { get; set; }
+
+        public Company()
+        {
+            Employees = new List<Individual>();
+        }
+    }
+}
---
+++ ContactManager/ContactManager.Core/Individual.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Core
+{
+    public class Individual : Contact
+    {
+        public Company Employer { get; set; }
+
+        public Individual()
+        {
+            Employer = null;
+        }
+    }
+}
