6eb2551b97e4392c1278726a1c0e699f83612de2
Silverback2023 <121978344+Silverback2023@users.noreply.github.com>
2023-07-29 02:01:22 -0700

    Add files via upload

---
+++ ContactManagerShared/ContactManagerShared/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagerShared
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
